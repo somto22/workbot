b24315eeb1fc827626ee64e8ecda642eb52051fa
somto22 <agusomtochukwu11@gmail.com>
2026-07-01 19:46:25 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -402,5 +402,5 @@ class NigerianAccountBot:
 target_url = "https://nnnrc.com/#/register"
 NUM_ACCOUNTS = 3
 
-bot = NigerianAccountBot(start_code=1104445)  # ← START FROM 0101621
+bot = NigerianAccountBot(start_code=1104461)  # ← START FROM 0101621
 bot.run(target_url, num_accounts=NUM_ACCOUNTS)
